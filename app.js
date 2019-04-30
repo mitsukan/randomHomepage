8d731412d11f334be2c3e69fab01c12e89439f93
mitsukan <jim_ip20@hotmail.com>
2019-04-30 13:38:51 +0100

    First working prototype: API called directly in HTML

--- app.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-import Unsplash from 'unsplash-js';
-
-const unsplash = new Unsplash({
-  applicationId: process.env.UNSPLASH_API_KEY,
-  secret: process.env.UNSPLASH_API_SECRET
-});
